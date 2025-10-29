189eddd2f3a9b9dd8eb8603c45662716256c3e2c
mandloinandani68-cyber <mandloinandani68@gmail.com>
2025-10-29 21:24:38 +0530

    Contract updated

--- contracts/MetaTransactionNetwork.sol
+++ contracts/MetaTransactionNetwork.sol
@@ -15,20 +15,8 @@
         return ecrecover(ethSignedHash, v, r, s);
     }
 
-    /// @dev Split bytes signature into r, s, v
-    function splitSignature(bytes memory sig)
-        internal
-        pure
-        returns (bytes32 r, bytes32 s, uint8 v)
-    {
-        require(sig.length == 65, "Invalid signature length");
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := byte(0, mload(add(sig, 96)))
-        }
-    }
-}
+    /update
+// 
 // 
 update
 // 
